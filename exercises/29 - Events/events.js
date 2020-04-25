9ecefadc65305f80387ec659d4d9f0ede984cef8
Huang <jjj08254@gmail.com>
2020-04-25 15:16:12 -0400

    changes exercises/29 events

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -2,52 +2,14 @@ const butts = document.querySelector('.butts');
 const coolButton = document.querySelector('.cool');
 
 function handleClick() {
-  console.log('🐛 IT GOT CLICKED!!!');
+  console.log('it is clicked');
 }
 
-const hooray = () => console.log('HOORAY!');
+const hooray = () => console.log('hooray');
 
-butts.addEventListener('click', function() {
-  console.log('Im an anon!');
-});
+butts.addEventListener('click', handleClick);
 coolButton.addEventListener('click', hooray);
 
-butts.removeEventListener('click', handleClick);
+const buyBottons = document.querySelectorAll('button.buy');
 
-// Listen on multiple items
-const buyButtons = document.querySelectorAll('button.buy');
-
-function handleBuyButtonClick(event) {
-  console.log('You clicked a button!');
-  const button = event.target;
-  // console.log(button.textContent);
-  // console.log(parseFloat(event.target.dataset.price));
-  console.log(event.target);
-  console.log(event.currentTarget);
-  console.log(event.target === event.currentTarget);
-  // Stop this event from bubbling up
-  // event.stopPropagation();
-}
-
-buyButtons.forEach(function(buyButton) {
-  buyButton.addEventListener('click', handleBuyButtonClick);
-});
-
-window.addEventListener(
-  'click',
-  function(event) {
-    console.log('YOU CLICKED THE WINDOW');
-    console.log(event.target);
-    console.log(event.type);
-    // event.stopPropagation();
-    console.log(event.bubbles);
-  },
-  { capture: true }
-);
-
-const photoEl = document.querySelector('.photo');
-
-photoEl.addEventListener('mouseenter', e => {
-  console.log(e.currentTarget);
-  console.log(this);
-});
+buyBottons.forEach(buyBotton => buyBotton.addEventListener('click', hooray));
