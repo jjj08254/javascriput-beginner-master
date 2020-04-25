346baefa3f299e5c46146fefce4c49ca961e195a
Huang <jjj08254@gmail.com>
2020-04-24 20:40:26 -0400

    changes exercises/20

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,8 @@
+const p = document.querySelector('p');
+const div = document.querySelectorAll('div');
+console.log(p);
+console.log(div);
+const item2Image = document.querySelector('.item2 img');
+console.log(item2Image)
+const item1Image2 = document.querySelector('.item [data-name=kait]');
+console.log(item1Image2)
