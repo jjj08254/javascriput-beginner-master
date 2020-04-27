f8c72416898d7697585c74e61bb38c6751de7aa0
Huang <jjj08254@gmail.com>
2020-04-27 18:03:49 -0400

    modified exercises/29 & 35 & 36

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,22 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    console.log('Removing Disabled');
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+  // payload[0].isIntersecting: whether it's on the page
+  // intersectionRatio: how many % is it on the page
+}
+
+// intersection observer
+const ob = new IntersectionObserver(obCallback, {
+  root: terms, // default is body
+  threshold: 1, // [0, 0.5, 1] trigger when the ratio is...
+});
+
+ob.observe(terms.lastElementChild);
--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,34 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(e) {
+  // hide all tab panels
+  tabPanels.forEach(panel => {
+    panel.hidden = true;
+  });
+  // make all tabs as unselected
+  tabButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false); // aria attribute has to use setAttribute
+  });
+  // make the clicked tab as selected
+  e.currentTarget.setAttribute('aria-selected', true);
+  // find the associated tabPanel and show it!
+  const { id } = e.currentTarget;
+
+  /*
+    METHOD 1
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"`);
+  tabPanel.hidden = false;
+  */
+
+  // METHOD 2: find in the array of tabPanels
+  const tabPanel = Array.from(tabPanels).find(panel => {
+    if (panel.getAttribute('aria-labelledby') === id) {
+      return true;
+    }
+  });
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
